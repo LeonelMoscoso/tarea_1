57243e3196e7b1066dd4602eb394e8a2d6b44e45
LeonelMoscoso <127239807+LeonelMoscoso@users.noreply.github.com>
2025-02-24 18:53:52 +0000

    carga setup

---
+++ src/prueba_2025/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Leonel Moscoso",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas"
+        "matplotlib"
+    ]
+)
